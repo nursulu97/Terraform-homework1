928909f0f493cbe416b030e5c207ef86c29d2984
nursulu97 <nursulu0797@gmail.com>
2024-04-10 18:09:40 +0000

    homework4

--- homework4/main.tf
+++ homework4/main.tf
@@ -11,3 +11,16 @@ resource "aws_instance" "Nursulu" {
     Name = "Nursulu"
   }
 }
+# # Terraform-homework4
+# i created 4 different .tfvars for 4 deffirent regions 
+# # For example this for ohio
+# region            = "us-east-2"
+# key_name          = "Bastion-key"
+# ports             = [22, 80, 443]
+# availability_zone = "us-east-2b"
+# instance_type     = "t2.micro"
+# ami_id            = "ami-0900fe555666598a2"
+# counts            = 1 
+# # And in command line typed 
+# #terraform apply -var-file="ohio.tfvars"
+# # in other regions we just changed variables for region, ami_id and availability zones
