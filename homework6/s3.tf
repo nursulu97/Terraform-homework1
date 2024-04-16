1183fe9f3640a05505e0ae74d0c9e4f1b53252cf
nursulu97 <nursulu0797@gmail.com>
2024-04-16 22:21:36 +0000

    homework6

--- homework6/s3.tf
+++ homework6/s3.tf
@@ -9,4 +9,12 @@ terraform {
 
 
 # Created manually S3 bucket  'nursulu-bucket' to send and keep the  state files
+# or we can create s3 bucket with terraform
+
+# resource "aws_s3_bucket" "terraform_state_bucket" {
+#     bucket = "nursulu-bucket"
+#     versioning {
+#       enabled = true
+# }
+#}
 # Created a DynamoDb  table named 'nursulu-lock' and with particion key name "LockID'
